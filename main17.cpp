1dcdf995c256109301711a01a1b575b9e9939027
ksandovalg8 <ksandovalg8@miumg.edu.gt>
2024-02-17 13:02:33 -0600

    karen cambio

---
+++ main17.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using na	mespace std;
+int main(int argc, char** argv) {
+	int a = 4;// declaramos e inicializamos una variable entera
+	int b = 1;// declaramos e inicializamos una variable entera 
+	
+	int suma = a+b;// realizamos la operacion 
+	
+	//imprimimos en pantalla el resultado de la suma 
+	cout<<"la suma es: "<<suma<<endl;
+	return 0;
+}
