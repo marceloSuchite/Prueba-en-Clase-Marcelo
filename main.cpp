60049528c2f618a78665df1bd804f9f23e1408b0
marceloSuchite <msuchiteg1@miumg.edu.gt>
2024-02-17 12:54:37 -0600

    Marcelo
    cambios por Marcelo

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+int a= 4; // declaramos e iniciamos una variable entera
+int b= 1; // declaramos e iniciamos una variable entera
+
+int suma = a+b; // realizamos la operacion
+
+// imprimimos en pantalla el resultado de la suma
+
+cout<< "la suma es: " <<suma<<endl;
+
+	return 0;
+}
